a8fddde9ee404d703375d3fe4facd9f601a7c959
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2024-05-01 19:52:22 +0530

    Update script2.js

--- public/js/script2.js
+++ public/js/script2.js
@@ -85,44 +85,17 @@ async function getSongs(folder){
   // Display the allbums
 
    async function displayallbums(){
-  
-    let a = await fetch(`/jainsongs/`)
+       currFolder = folder;
+   let a = await fetch(`/${folder}/`)
     let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardContainer")
-  let array = Array.from(anchors)
-     console.log(e.href)
-      for (let index = 0; index < array.length; index++) {
-        const e = array[index];    
-    if(e.href.includes("/jainsongs/") && !e.href.includes(".htaccess")){
- 
-    let folder= e.href.split("/").slice(4)[0]
-    let response = await a.json();
-    console.log(response)
-
-     cardContainer.innerHTML =cardContainer.innerHTML + `<div data-floder="${folder}" class="card ">
-     <div class="play">
-     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35" color="Black" fill="#1fdf64">
-             <circle cx="12" cy="12" r="10" stroke="#1fdf64" stroke-width="1.5" />
-             <path d="M15.4531 12.3948C15.3016 13.0215 14.5857 13.4644 13.1539 14.3502C11.7697 15.2064 11.0777 15.6346 10.5199 15.4625C10.2893 15.3913 10.0793 15.2562 9.90982 15.07C9.5 14.6198 9.5 13.7465 9.5 12C9.5 10.2535 9.5 9.38018 9.90982 8.92995C10.0793 8.74381 10.2893 8.60868 10.5199 8.53753C11.0777 8.36544 11.7697 8.79357 13.1539 9.64983C14.5857 10.5356 15.3016 10.9785 15.4531 11.6052C15.5156 11.8639 15.5156 12.1361 15.4531 12.3948Z" stroke="currentColor" fill="#000"stroke-width="1.5" stroke-linejoin="round" />
-         </svg>
- </div>
-     <img src="/jainsongs/${folder}/cover.jpg" alt="">
-     <h2>${response.title}</h2>
-     <p> ${response.description}</p>             
-</div>    
-     
-     `
-    }
-
-
-    }
-
-  }
   
- 
+    let div = document.createElement("div")
+    div.innerHTML=response;
+    console.log(div) 
+
+   }
+  
+  
      
  async function main(){
  
